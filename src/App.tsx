0b283513202a96f27c792c502c90f1899ff2e00f
Meiyi <ant19950601@gmail.com>
2022-09-22 23:49:27 +0800

    :truck: move redux file

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,3 @@
-import React from "react";
-import logo from "./logo.svg";
 import { Counter } from "./features/counter/Counter";
 import "./App.scss";
 
@@ -7,7 +5,6 @@ function App() {
    return (
       <div className="App">
          <header className="App-header">
-            <img src={logo} className="App-logo" alt="logo" />
             <Counter />
             <p>
                Edit <code>src/App.tsx</code> and save to reload.
