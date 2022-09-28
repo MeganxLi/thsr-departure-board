5ea5dfadea9f04b1bc03853aabf2febc7b4908fc
Meiyi <ant19950601@gmail.com>
2022-09-28 22:16:47 +0800

    :memo: fix type

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,19 @@ import { Counter } from "./features/counter/Counter";
 import DepartureBoard from "./pages/DepartureBoard";
 import "./styles/App.scss";
 import { useGetAuthorizationMutation } from "./services/API";
-import { TokenType } from "./utils/Type";
 
 function App() {
-    const [getAuthorization, { isError, error }] = useGetAuthorizationMutation();
+   const [getAuthorization, { isError, error }] = useGetAuthorizationMutation();
 
-    useEffect(() => {
-        getAuthorization(null);
-    }, []);
+   useEffect(() => {
+      getAuthorization(null);
+   }, []);
 
-    return (
-        <div className="App">
-            <DepartureBoard />
-        </div>
-    );
+   return (
+      <div className="App">
+         <DepartureBoard />
+      </div>
+   );
 }
 
 export default App;
--- src/services/API.tsx
+++ src/services/API.tsx
@@ -1,42 +1,41 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import axios from "axios";
 import qs from "qs";
-import { ParameterType, TokenType } from "../utils/Type";
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({
-        baseUrl: "https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token",
-        prepareHeaders: (headers, api) => {
-            headers.set("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
-            return headers;
-        },
-    }),
-    endpoints: () => ({}),
+   baseQuery: fetchBaseQuery({
+      baseUrl: "https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token",
+      prepareHeaders: (headers, api) => {
+         headers.set("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+         return headers;
+      },
+   }),
+   endpoints: () => ({}),
 });
 
 //分離 API
 const noStoreApi = api.injectEndpoints({
-    endpoints: (builder) => ({
-        // 獲取token
-        getAuthorization: builder.mutation<TokenType, any>({
-            query: () => {
-                return {
-                    url: "",
-                    method: "POST",
-                    // body: {
-                    //     grant_type: "client_credentials",
-                    //     client_id: "aa147b258tw-101800a9-2ba1-4051",
-                    //     client_secret: "6249cf42-79c7-4fc6-8598-c63d4281c883",
-                    // },
-                    body: qs.stringify({
-                        grant_type: "client_credentials",
-                        client_id: "aa147b258tw-101800a9-2ba1-4051",
-                        client_secret: "6249cf42-79c7-4fc6-8598-c63d4281c883",
-                    }),
-                };
-            },
-        }),
-    }),
+   endpoints: (builder) => ({
+      // 獲取token
+      getAuthorization: builder.mutation<TokenType, any>({
+         query: () => {
+            return {
+               url: "",
+               method: "POST",
+               // body: {
+               //     grant_type: "client_credentials",
+               //     client_id: "aa147b258tw-101800a9-2ba1-4051",
+               //     client_secret: "6249cf42-79c7-4fc6-8598-c63d4281c883",
+               // },
+               body: qs.stringify({
+                  grant_type: "client_credentials",
+                  client_id: "aa147b258tw-101800a9-2ba1-4051",
+                  client_secret: "6249cf42-79c7-4fc6-8598-c63d4281c883",
+               }),
+            };
+         },
+      }),
+   }),
 });
 
 export const { useGetAuthorizationMutation } = noStoreApi;
