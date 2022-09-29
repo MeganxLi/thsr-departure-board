e4c077fd34557486d05d8692820097b8e09d3800
Meiyi <ant19950601@gmail.com>
2022-09-29 23:53:43 +0800

    meger stassh api

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,22 @@ import { useEffect, useState } from "react";
 import { Counter } from "./features/counter/Counter";
 import DepartureBoard from "./pages/DepartureBoard";
 import "./styles/App.scss";
-import { useGetAuthorizationMutation } from "./services/API";
+import { useGetAuthorizationMutation, useGetStationQuery } from "./services/API";
 
 function App() {
-   const [getAuthorization, { isError, error }] = useGetAuthorizationMutation();
+    const [getAuthorization, { isError, error }] = useGetAuthorizationMutation();
+    const { data } = useGetStationQuery({});
 
-   useEffect(() => {
-      getAuthorization(null);
-   }, []);
+    useEffect(() => {
+        getAuthorization(null);
+        console.log(data);
+    }, []);
 
-   return (
-      <div className="App">
-         <DepartureBoard />
-      </div>
-   );
+    return (
+        <div className="App">
+            <DepartureBoard />
+        </div>
+    );
 }
 
 export default App;
--- src/services/API.tsx
+++ src/services/API.tsx
@@ -3,39 +3,38 @@ import axios from "axios";
 import qs from "qs";
 
 export const api = createApi({
-   baseQuery: fetchBaseQuery({
-      baseUrl: "https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token",
-      prepareHeaders: (headers, api) => {
-         headers.set("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
-         return headers;
-      },
-   }),
-   endpoints: () => ({}),
+    baseQuery: fetchBaseQuery({
+        baseUrl: "",
+        prepareHeaders: (headers, api) => {
+            headers.set("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+            return headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        // 獲取token
+        getAuthorization: builder.mutation<TokenType, any>({
+            query: () => {
+                return {
+                    url: "https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token",
+                    method: "POST",
+                    body: qs.stringify({
+                        grant_type: "client_credentials",
+                        client_id: process.env.REACT_APP_CLIENT_ID,
+                        client_secret: process.env.REACT_APP_CLIENT_SECRET,
+                    }),
+                };
+            },
+        }),
+        getStation: builder.query<StationType[], any>({
+            // 取得高鐵車站基本資料
+            query: () => {
+                return {
+                    url: "https://tdx.transportdata.tw/api/basic/v2/Rail/THSR/Station",
+                    method: "GET",
+                };
+            },
+        }),
+    }),
 });
 
-//分離 API
-const noStoreApi = api.injectEndpoints({
-   endpoints: (builder) => ({
-      // 獲取token
-      getAuthorization: builder.mutation<TokenType, any>({
-         query: () => {
-            return {
-               url: "",
-               method: "POST",
-               // body: {
-               //     grant_type: "client_credentials",
-               //     client_id: "aa147b258tw-101800a9-2ba1-4051",
-               //     client_secret: "6249cf42-79c7-4fc6-8598-c63d4281c883",
-               // },
-               body: qs.stringify({
-                  grant_type: "client_credentials",
-                  client_id: "aa147b258tw-101800a9-2ba1-4051",
-                  client_secret: "6249cf42-79c7-4fc6-8598-c63d4281c883",
-               }),
-            };
-         },
-      }),
-   }),
-});
-
-export const { useGetAuthorizationMutation } = noStoreApi;
+export const { useGetAuthorizationMutation, useGetStationQuery } = api;
