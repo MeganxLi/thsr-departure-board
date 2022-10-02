6bca1451d312a57a9eee4186a65cfeb2334da396
Meiyi <ant19950601@gmail.com>
2022-10-02 23:17:08 +0800

    :memo: update departue and app file

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,14 @@ import "./styles/App.scss";
 import { useGetAuthorizationMutation, useGetStationQuery } from "./services/API";
 
 function App() {
-    const [getAuthorization, { isError, error }] = useGetAuthorizationMutation();
-    const { data } = useGetStationQuery({});
+   const [getAuthorization, { isError, error }] = useGetAuthorizationMutation();
+   const { data } = useGetStationQuery({});
 
-    useEffect(() => {
-        getAuthorization(null);
-        console.log(data);
-    }, []);
-
-    return (
-        <div className="App">
-            <DepartureBoard />
-        </div>
-    );
+   return (
+      <div className="App">
+         <DepartureBoard />
+      </div>
+   );
 }
 
 export default App;
--- src/pages/DepartureBoard.tsx
+++ src/pages/DepartureBoard.tsx
@@ -1,9 +1,34 @@
+import { useDispatch, useSelector } from "react-redux";
+import Marquee from "../components/Marquee";
 import TrainTime from "../components/TrainTime";
+import { useGetDesignatedStationQuery } from "../services/API";
+import { RootState } from "../store";
+import AnalogClock from "../components/AnalogClock";
 
 const DepartureBoard = () => {
+   const dispatch = useDispatch();
+   const stationList = useSelector((state: RootState) => state.base.getStationList);
+   const selectStation = useSelector((state: RootState) => state.base.selectStation);
+   // const { data } = useGetDesignatedStationQuery(selectStation);
+
+   const getSelectTrain = (e: any) => {
+      console.log(e.target.value);
+   };
+
    return (
       <div>
+         <select onChange={getSelectTrain}>
+            {stationList.map((TrainName: StationType, idx) => {
+               return (
+                  <option key={idx} value={TrainName.StationID}>
+                     {TrainName.StationName.Zh_tw}
+                  </option>
+               );
+            })}
+         </select>
          <TrainTime />
+         <Marquee />
+         <AnalogClock />
       </div>
    );
 };
