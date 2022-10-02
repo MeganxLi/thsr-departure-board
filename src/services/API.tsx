04e821e8db125ec7c57982c78ea83cd6d938aad6
Meiyi <ant19950601@gmail.com>
2022-10-02 23:16:38 +0800

    :memo: update get designated station api

--- src/services/API.tsx
+++ src/services/API.tsx
@@ -3,38 +3,47 @@ import axios from "axios";
 import qs from "qs";
 
 export const api = createApi({
-    baseQuery: fetchBaseQuery({
-        baseUrl: "",
-        prepareHeaders: (headers, api) => {
-            headers.set("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
-            return headers;
-        },
-    }),
-    endpoints: (builder) => ({
-        // 獲取token
-        getAuthorization: builder.mutation<TokenType, any>({
-            query: () => {
-                return {
-                    url: "https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token",
-                    method: "POST",
-                    body: qs.stringify({
-                        grant_type: "client_credentials",
-                        client_id: process.env.REACT_APP_CLIENT_ID,
-                        client_secret: process.env.REACT_APP_CLIENT_SECRET,
-                    }),
-                };
-            },
-        }),
-        getStation: builder.query<StationType[], any>({
-            // 取得高鐵車站基本資料
-            query: () => {
-                return {
-                    url: "https://tdx.transportdata.tw/api/basic/v2/Rail/THSR/Station",
-                    method: "GET",
-                };
-            },
-        }),
-    }),
+   baseQuery: fetchBaseQuery({
+      baseUrl: "",
+      prepareHeaders: (headers, api) => {
+         headers.set("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+         return headers;
+      },
+   }),
+   endpoints: (builder) => ({
+      // 獲取token
+      getAuthorization: builder.mutation<TokenType, any>({
+         query: () => {
+            return {
+               url: "https://tdx.transportdata.tw/auth/realms/TDXConnect/protocol/openid-connect/token",
+               method: "POST",
+               body: qs.stringify({
+                  grant_type: "client_credentials",
+                  client_id: process.env.REACT_APP_CLIENT_ID,
+                  client_secret: process.env.REACT_APP_CLIENT_SECRET,
+               }),
+            };
+         },
+      }),
+      getStation: builder.query<StationType[], any>({
+         // 取得高鐵車站基本資料
+         query: () => {
+            return {
+               url: "https://tdx.transportdata.tw/api/basic/v2/Rail/THSR/Station",
+               method: "GET",
+            };
+         },
+      }),
+      getDesignatedStation: builder.query<DesignatedStationType[], selectStationType>({
+         // 取得指定車站時刻表
+         query: ({ StationID, TrainDate }) => {
+            return {
+               url: `https://tdx.transportdata.tw/api/basic/v2/Rail/THSR/DailyTimetable/Station/${StationID}/${TrainDate}`,
+               method: "GET",
+            };
+         },
+      }),
+   }),
 });
 
-export const { useGetAuthorizationMutation, useGetStationQuery } = api;
+export const { useGetAuthorizationMutation, useGetStationQuery, useGetDesignatedStationQuery } = api;
--- src/store/baseSlice.tsx
+++ src/store/baseSlice.tsx
@@ -3,19 +3,28 @@ import { api } from "../services/API";
 
 const initialState = {
    getStationList: [] as StationType[],
+   selectStation: {
+      StationID: "1035",
+      TrainDate: "2022-10-01",
+   },
+   getDesignatedStation: [] as DesignatedStationType[],
 };
 
 export const baseSlice = createSlice({
    name: "base",
    initialState,
    reducers: {
-      stationList: (state, action) => {
-         console.log(state, action);
+      stationList: (state, action) => {},
+      selectStation: (state, action) => {
+         console.log("selectStation", state, action);
       },
    },
    extraReducers: (builder) => {
       builder.addMatcher(api.endpoints.getStation.matchFulfilled, (state, { payload }) => {
          state.getStationList = payload;
+      });
+      builder.addMatcher(api.endpoints.getDesignatedStation.matchFulfilled, (state, { payload }) => {
+         state.getDesignatedStation = payload;
       });
    },
 });
