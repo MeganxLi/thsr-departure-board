a9be61d6b4a4ea07978ca8c3b4f2c6ebe8002cc3
Meiyi <ant19950601@gmail.com>
2022-10-05 11:19:07 +0800

    :bug: fix found store api bug

--- src/index.tsx
+++ src/index.tsx
@@ -1,14 +1,13 @@
-import React from "react";
 import { createRoot } from "react-dom/client";
 import { Provider } from "react-redux";
-import { store } from "./store/index";
 import App from "./App";
+import store from "./store";
 
 const container = document.getElementById("root")!;
 const root = createRoot(container);
 
 root.render(
-   <Provider store={store}>
-      <App />
-   </Provider>
+    <Provider store={store}>
+        <App />
+    </Provider>
 );
--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,10 +1,15 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { api } from "../services/API";
 import baseReducer from "./baseSlice";
 
-export const store = configureStore({
-   reducer: {
-      base: baseReducer,
-   },
+const store = configureStore({
+    reducer: {
+        [api.reducerPath]: api.reducer,
+        base: baseReducer,
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
 });
 
 export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch;
+export default store;
