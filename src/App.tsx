5c239aaa6e131b1260901a53de4ecfb69464fca2
Meiyi <ant19950601@gmail.com>
2022-10-12 22:48:49 +0800

    :truck: move files

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,18 @@
 import { useEffect, useState } from "react";
 import { Counter } from "./features/counter/Counter";
-import DepartureBoard from "./pages/DepartureBoard";
 import "./styles/App.scss";
 import { useGetAuthorizationMutation, useGetStationQuery } from "./services/API";
+import DepartureBoard from "./pages";
 
 function App() {
-   const [getAuthorization, { isError, error }] = useGetAuthorizationMutation();
-   const { data } = useGetStationQuery({});
+  const [getAuthorization, { isError, error }] = useGetAuthorizationMutation();
+  const { data } = useGetStationQuery({});
 
-   return (
-      <div className="App">
-         <DepartureBoard />
-      </div>
-   );
+  return (
+    <div className="App">
+      <DepartureBoard />
+    </div>
+  );
 }
 
 export default App;
