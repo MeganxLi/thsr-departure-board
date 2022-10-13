5f8e556e9b42b096b4e552f1ccb6019bc17594bd
Meiyi <ant19950601@gmail.com>
2022-10-14 00:27:07 +0800

    :bug: fix tw date

--- src/utils/dataprocessor.tsx
+++ src/utils/dataprocessor.tsx
@@ -1,4 +1,4 @@
-export const getToday = new Date().toISOString().substring(0, 10); // 取得今天日期轉 yyyy-mm-dd 
+export const getToday = new Date().toLocaleDateString('zh-TW').replace(/\//g, "-"); // 取得今天日期轉 yyyy-mm-dd
 
 export const getNowTime = new Date().toLocaleString('zh-TW', {
   hour12: false, hour: "2-digit", minute: "2-digit"
