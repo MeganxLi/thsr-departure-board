8d98878de219cb0c14f28f6dd15ee5a37cfb7411
Meiyi <ant19950601@gmail.com>
2022-10-14 00:37:38 +0800

    :bug: fix get now time 24hr

--- src/utils/dataprocessor.tsx
+++ src/utils/dataprocessor.tsx
@@ -1,5 +1,5 @@
 export const getToday = new Date().toLocaleDateString('zh-TW').replace(/\//g, "-"); // 取得今天日期轉 yyyy-mm-dd
 
 export const getNowTime = new Date().toLocaleString('zh-TW', {
-  hour12: false, hour: "2-digit", minute: "2-digit"
+  hourCycle: 'h23', hour: "2-digit", minute: "2-digit",
 });; // 取得現在時間 hh:mm
