928856ca39f1bbe71b4fca5bb5c9ee740f344d3b
Meiyi <ant19950601@gmail.com>
2022-10-18 23:58:03 +0800

    :memo: add redux AppDispatch TypeScript

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -1,12 +1,14 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { useDispatch } from "react-redux";
 import { api } from "../services/API";
 import rootReducer from "./rootReducer";
 
 const store = configureStore({
-    reducer: rootReducer,
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
+  reducer: rootReducer,
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
 });
+export default store;
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-export default store;
+export const useAppDispatch: () => AppDispatch = useDispatch;
