e9058c3cf00f859bed5ea200d91ab5186ceef007
Meiyi <ant19950601@gmail.com>
2022-10-19 15:44:31 +0800

    :heavy_plus_sign: add useAppDispatch & useAppSelector

--- src/store/index.tsx
+++ src/store/index.tsx
@@ -7,8 +7,7 @@ const store = configureStore({
   reducer: rootReducer,
   middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(api.middleware),
 });
-export default store;
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
-export const useAppDispatch: () => AppDispatch = useDispatch;
+export default store;
