d23f3bd0b3fa4dbd91af6545d963ae98417e7f1a
Meiyi <ant19950601@gmail.com>
2022-10-24 23:04:56 +0800

    :lipstick: add nav style

--- src/components/Nav.tsx
+++ src/components/Nav.tsx
@@ -1,13 +1,15 @@
 import routes from '../route/router'
-import { Link } from 'react-router-dom'
+import { Link, useLocation } from 'react-router-dom'
 
 const Nav = () => {
+  const pathname = useLocation().pathname
+
   return (
     <nav>
       <ul>
         {routes.map((pages: PageListType, idx: number) => {
           return (
-            <li key={idx}>
+            <li key={idx} className={pages.path === pathname ? 'nav-action' : undefined}>
               <Link to={pages.path}>{pages.name}</Link>
             </li>
           )
