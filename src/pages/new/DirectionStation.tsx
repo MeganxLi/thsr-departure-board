29657f69f7ff2c85e3e43a35ffaa6610fedebae2
Meiyi <ant19950601@gmail.com>
2022-10-27 23:46:34 +0800

    :memo: update new direction station files

--- src/pages/new/DirectionStation.tsx
+++ src/pages/new/DirectionStation.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
 import { TrainInfoTitle } from '../../constants/Messages'
+import { StationArrayLength } from '../../constants/NormType'
 import { useAppSelector } from '../../store/hook'
+import { StyledNewTrainTime } from '../../styled/DirectionStation'
+import { getNowTime } from '../../utils/dataprocessor'
 import Stepper from './Stepper'
 
 interface props {
@@ -18,23 +20,47 @@ const DirectionStation = ({ Direction }: props) => {
         {Object.values(TrainInfoTitle).map((title: string, idx: number) =>
           <span key={idx}> {title}</span>
         )}
-        <div className='new-stop-station-title'>
+        <div className='new-stop-station'>
           {getStationsList.map((list: StationType, idx: number) =>
             <span key={idx}>{list.StationName.Zh_tw}</span>
           )}
         </div>
       </div>
       {(Direction ? getNewNorthDesignatedStation : getNewSouthDesignatedStation).map((station: DesignatedStationType, idx: number) => {
+        let comingSoonTime: boolean = false // 是否即將到站時間
+
+        // 即將到時間接近2分鐘才變色
+        comingSoonTime = (parseInt(getNowTime().replace(':', '')) - parseInt(station.DepartureTime.replace(':', '')) >= -2)
+        console.log(parseInt(getNowTime().replace(':', '')) - parseInt(station.DepartureTime.replace(':', '')), comingSoonTime)
+
         return (
-          <div key={idx} className='new-station'>
+          <StyledNewTrainTime
+            key={idx}
+            className='new-station'
+            $direction={Direction}
+            $comingSoon={comingSoonTime}
+          >
             <span>{station.TrainNo}</span>
             <span>{station.EndingStationName.Zh_tw}</span>
             <span>{station.DepartureTime}</span>
             <span>9-10</span>
-            <Stepper TrainNo={station.TrainNo} />
-          </div>
+            <Stepper Direction={Direction} getStationsList={getStationsList} TrainNo={station.TrainNo} />
+          </StyledNewTrainTime>
         )
       })}
+      { // 補齊剩餘陣列
+        Array(StationArrayLength - (Direction ? getNewNorthDesignatedStation.length : getNewSouthDesignatedStation.length))
+          .fill('')
+          .map((item: any, idx: number) =>
+            <StyledNewTrainTime
+              key={idx}
+              className='new-station'
+              $direction={Direction}
+              $comingSoon={false}
+            >
+            </StyledNewTrainTime>
+          )
+      }
 
     </div>
   )
--- src/pages/new/Stepper.tsx
+++ src/pages/new/Stepper.tsx
@@ -5,15 +5,16 @@ import { StyleNewStepper } from '../../styled/Stepper'
 import { handleStopStationArray } from '../../utils/dataprocessor'
 
 interface props {
+  Direction: boolean
+  getStationsList: StationType[]
   TrainNo: string
+
 }
 
-const Stepper = ({ TrainNo }: props) => {
-  const { getStationList } = useAppSelector((state) => state.base)
+const Stepper = ({ Direction, getStationsList, TrainNo }: props) => {
+  const { selectStationName } = useAppSelector((state) => state.base)
   const { data } = useGetTrainNoInfoQuery({ TrainNo })
   const [StopStation, setStopStation] = useState<string[]>([])
-
-  console.log('Stepper', data)
 
   useEffect(() => {
     if (data != null) {
@@ -22,18 +23,28 @@ const Stepper = ({ TrainNo }: props) => {
   }, [data])
 
   return (
-    <div id="New_Stepper">
-      {getStationList.map((item: StationType, idx: number) => {
+    <div id="New_Stepper" className='new-stop-station'>
+      {getStationsList.map((item: StationType, idx: number) => {
+        const SelectStation = selectStationName === item.StationName.Zh_tw
+        const LastStation = StopStation[StopStation.length - 1] === item.StationName.Zh_tw
+
         return (
           <StyleNewStepper
             key={idx}
-            $stopStation={StopStation.includes(item.StationName.Zh_tw)}>
-            O
+            idx={idx}
+            $direction={Direction}
+            $hiddenStation={!StopStation.includes(item.StationName.Zh_tw)}
+            $sameStation={SelectStation}
+            className={`${SelectStation ? 'select-station' : ''} ${LastStation ? 'last-station' : ''}`}
+            listLength={getStationsList.length}
+          >
+            <span className='new-station-route-left'></span>
+            <span className='new-station-route-right'></span>
           </StyleNewStepper>
         )
       })}
 
-    </div>
+    </div >
   )
 }
 
